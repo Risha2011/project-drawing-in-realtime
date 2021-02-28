85b5d4da89191ec4b94692b0917df8ddf36fef7e
Risha2011 <74859480+Risha2011@users.noreply.github.com>
2021-02-28 14:38:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-function setup(){
-    video = createCapture(VIDEO);
-    video.size(550, 500);
+rightWristX = 0;
+leftWristX = 0;
 
-    canvas = createCanvas(550, 500);
-    canvas.position(560, 150);
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
-function draw(){
-    background('#918380');
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
 }
-function modelLoaded(){
-    console.log('PoseNet Is Initialised !');
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX);
+  }
 }
-function gotPoses(){
-    if(results.length > 0){
-        console.log(results);
-    }
+
+function draw() {
+background('#969A97');
+
+  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+  fill('#F90093');
+  stroke('#F90093');
+  square(100);
 }
